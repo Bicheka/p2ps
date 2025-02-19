4f0b3737903f80b7fc8d541701685a626c797113
bicheka <davidmartinezgilp@gmail.com>
2025-02-18 19:18:47 -0500

    feat: add P2ps trait in p2p_sync mod

--- src/p2p_sync.rs
+++ src/p2p_sync.rs
@@ -4,16 +4,38 @@ use aes_gcm::{Aes256Gcm, Key};
 
 use crate::P2ps;
 
-impl<T: Read + Write> P2ps<T> {
-    fn new_sync(stream: T, key: Key<Aes256Gcm>) -> Self {
-        Self { stream, key }
-    }
+trait P2psSync<T>: Sized
+where
+    T: Read + Write,
+{
+    fn listen_handshake(mut stream: T) -> std::io::Result<Self>;
+    fn send_handshake(mut stream: T) -> std::io::Result<Self>;
+    fn new(stream: T, key: Key<Aes256Gcm>) -> Self;
+    fn write(&mut self, data: &[u8]) -> std::io::Result<()>;
+    fn read(&mut self) -> std::io::Result<Vec<u8>>;
+}
 
-    pub fn send_encrypted(&mut self, data: &[u8]) -> std::io::Result<()> {
+impl<T> P2psSync<T> for P2ps<T>
+where
+    T: Read + Write,
+{
+    fn listen_handshake(mut stream: T) -> std::io::Result<Self> {
         todo!()
     }
 
-    pub fn recieve_encrypted(&mut self) -> std::io::Result<Vec<u8>> {
+    fn send_handshake(mut stream: T) -> std::io::Result<Self> {
+        todo!()
+    }
+
+    fn new(stream: T, key: Key<Aes256Gcm>) -> Self {
+        todo!()
+    }
+
+    fn write(&mut self, data: &[u8]) -> std::io::Result<()> {
+        todo!()
+    }
+
+    fn read(&mut self) -> std::io::Result<Vec<u8>> {
         todo!()
     }
 }
