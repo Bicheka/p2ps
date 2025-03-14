5345065f9b9ed15e952adfe1c9374e6763010df5
xadaemon <soft.song@fastmail.net>
2025-03-14 17:19:05 +0100

    dev: Add custom Error type using thiserror and use it to remove possible panic points in the code.

--- src/p2p_sync.rs
+++ src/p2p_sync.rs
@@ -1,5 +1,6 @@
 use crate::common::{Encryption, Keys};
-use aes_gcm::{aead::Aead, Aes256Gcm, Key, KeyInit, Nonce};
+use crate::{Error, Result};
+use aes_gcm::{aead::Aead, Aes256Gcm, Key};
 use std::io::{Read, Write};
 
 /// Handles encrypted P2P communication.
@@ -12,20 +13,12 @@ impl<T> Encryption for P2psConn<T>
 where
     T: Read + Write,
 {
-    fn encrypt(&self, input_data: &[u8]) -> (Vec<u8>, [u8; 12]) {
-        let nonce = [0u8; 12];
-        let cipher = Aes256Gcm::new(&self.key);
-        let encrypted_data = cipher
-            .encrypt(&nonce.into(), input_data)
-            .expect("Error encrypting data");
-        (encrypted_data, nonce)
+    fn encrypt(&self, input_data: &[u8]) -> Result<(Vec<u8>, [u8; 12])> {
+        crate::p2ps_conn_common::encrypt(&self.key, input_data)
     }
 
-    fn decrypt(&self, encrypted_data: &[u8], nonce: &[u8; 12]) -> Vec<u8> {
-        let cipher = Aes256Gcm::new(&self.key);
-        cipher
-            .decrypt(Nonce::from_slice(nonce), encrypted_data)
-            .expect("decryption failed")
+    fn decrypt(&self, encrypted_data: &[u8], nonce: &[u8; 12]) -> Result<Vec<u8>> {
+        crate::p2ps_conn_common::decrypt(&self.key, encrypted_data, nonce)
     }
 }
 
@@ -34,10 +27,10 @@ where
     T: Read + Write,
 {
     /// Listens for an incoming handshake and sends back a public key and creates a P2psConnAsync
-    pub fn listen_handshake(mut stream: T) -> std::io::Result<Self> {
+    pub fn listen_handshake(mut stream: T) -> Result<Self> {
         // receive their public key
         let mut buffer = [0u8; 32];
-        stream.read(&mut buffer)?;
+        stream.read_exact(&mut buffer)?;
 
         // generate private and public keys
         let keys = Keys::generate_keys();
@@ -46,13 +39,13 @@ where
         stream.write_all(&keys.get_public_key_bytes())?;
 
         // create encryption key with private key and their public key
-        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer)?);
+        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer)?)?;
         // create P2ps
         Ok(Self { stream, key })
     }
 
     /// Sends handshake to a peer and uses peer response to construct a P2psConn
-    pub fn send_handshake(mut stream: T) -> std::io::Result<Self> {
+    pub fn send_handshake(mut stream: T) -> Result<Self> {
         // generate public and private keys
         let keys = Keys::generate_keys();
 
@@ -61,18 +54,18 @@ where
 
         // listen for response with their public key
         let mut buffer = [0u8; 32];
-        stream.read(&mut buffer)?;
+        stream.read_exact(&mut buffer)?;
 
         // generate encryption key with the private key and their public key
-        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer)?);
+        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer)?)?;
 
         // Create a P2psConn
         Ok(Self { stream, key })
     }
 
     /// takes data, encrypts it and sends it to the peer
-    pub fn write(&mut self, data: &[u8]) -> std::io::Result<()> {
-        let (encrypted_data, nonce) = self.encrypt(data);
+    pub fn write(&mut self, data: &[u8]) -> Result<()> {
+        let (encrypted_data, nonce) = self.encrypt(data)?;
 
         self.stream.write_all(&nonce)?;
 
@@ -86,7 +79,7 @@ where
     }
 
     /// Reads data from a stream decrypts it returning the data and len
-    fn read_len(&mut self) -> std::io::Result<(Vec<u8>, usize)> {
+    fn read_len(&mut self) -> Result<(Vec<u8>, usize)> {
         // Read nonce
         let mut nonce_buf = [0u8; 12];
         self.stream.read_exact(&mut nonce_buf)?;
@@ -100,31 +93,33 @@ where
         let mut encrypted_data = vec![0u8; length];
         self.stream.read_exact(&mut encrypted_data)?;
 
-        let data = self.decrypt(&encrypted_data, &nonce_buf);
+        let data = self.decrypt(&encrypted_data, &nonce_buf)?;
 
         Ok((data, length))
     }
 
     /// Reads data from a stream decrypts it returning the data and len
-    pub fn read(&mut self) -> std::io::Result<Vec<u8>> {
+    pub fn read(&mut self) -> Result<Vec<u8>> {
         let (data, _) = self.read_len()?;
         Ok(data)
     }
 
     /// Reads data from a stream decrypts it then writes it to a provided slice.
     /// The slice will remain unmodified if any error occurs.
-    pub fn read_to_slice(&mut self, slice: &mut [u8]) -> std::io::Result<()> {
+    pub fn read_to_slice(&mut self, slice: &mut [u8]) -> Result<()> {
         let (data, len) = self.read_len()?;
         if len <= slice.len() {
             slice[..len].copy_from_slice(&data[..len]);
             Ok(())
         } else {
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "Provided slice cannot fit data from read"))
+            Err(Error::Other(
+                "Provided slice cannot fit data from read".to_string(),
+            ))
         }
     }
 
     /// Reads data from a stream decrypts it then writes it to a Vec
-    pub fn read_to_buf(&mut self, buf: &mut Vec<u8>) -> std::io::Result<()> {
+    pub fn read_to_buf(&mut self, buf: &mut Vec<u8>) -> Result<()> {
         let (data, _) = self.read_len()?;
         buf.extend(data);
         Ok(())
