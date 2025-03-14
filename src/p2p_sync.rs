72417a7b61a95172d9a6f3378433704c3bed9f52
Matheus Xavier <soft.song@fastmail.net>
2025-03-14 23:52:30 +0100

    Update src/p2p_sync.rs
    Co-authored-by: David Martinez Gil <davidmartinezgilp@gmail.com>

--- src/p2p_sync.rs
+++ src/p2p_sync.rs
@@ -39,7 +39,7 @@ where
         stream.write_all(&keys.get_public_key_bytes())?;
 
         // create encryption key with private key and their public key
-        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer)?)?;
+        let key = keys.generate_encryption_key(&Keys::public_key_from_bytes(buffer))?;
         // create P2ps
         Ok(Self { stream, key })
     }
