9feda1518d4ede1bdbd9aee704c9386c4f356da9
bicheka <davidmartinezgilp@gmail.com>
2025-06-22 18:47:24 -0400

    Fixes #14 - added unique nonce generation

--- src/secure_conn/encription.rs
+++ src/secure_conn/encription.rs
@@ -1,6 +1,8 @@
 use crate::{Error, Result};
 use aes_gcm::aead::Aead;
 use aes_gcm::{Aes256Gcm, Key, KeyInit, Nonce};
+use rand::rngs::OsRng;
+use rand::RngCore;
 
 pub(crate) trait Encryption {
     fn encrypt(&self, input_data: &[u8]) -> Result<(Vec<u8>, [u8; 12])>;
@@ -11,12 +13,12 @@ pub(crate) fn encrypt(
     key: &Key<Aes256Gcm>,
     input_data: &[u8],
 ) -> crate::Result<(Vec<u8>, [u8; 12])> {
-    let nonce = [0u8; 12];
-    let cipher = Aes256Gcm::new(&key);
-    let encrypted_data = match cipher.encrypt(&nonce.into(), input_data) {
-        Ok(v) => v,
-        Err(e) => return Err(Error::CryptError(e)),
-    };
+    let mut nonce = [0u8; 12];
+    OsRng.fill_bytes(&mut nonce);
+    let cipher = Aes256Gcm::new(key);
+    let encrypted_data = cipher
+        .encrypt(Nonce::from_slice(&nonce), input_data)
+        .map_err(Error::CryptError)?;
     Ok((encrypted_data, nonce))
 }
 
